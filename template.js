974b8495fe0a40c987f8ba00ca235c81f95d80e1
mamun-prodhan <mamunpr9@gmail.com>
2023-08-24 09:21:27 +0600

    template string

---
+++ template.js
@@ -0,0 +1,8 @@
+const numbers = [45, 20, 25];
+const student = {
+    name: 'Mamun Prodhan',
+    age: 5
+}
+
+const result = `The sum of ${numbers[1]} and ${student.age} is = ${numbers[1] + student.age}`
+console.log(result);
